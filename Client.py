1e99fe3701dea206880dab87ab9cb16178fc95e8
daelHacohen <dael12345@gmail.com>
2024-12-30 15:42:51 +0200

    update 2

--- Client.py
+++ Client.py
@@ -3,8 +3,17 @@ def client():
     client_socket =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 12345))
     print("connected to the server")
+
+    client_socket.send("GET_MAX_MSG_SIZE".encode())#מבקש את גודל ההודעה המקסימלי
+    max_msg_size = int(client_socket.recv(1024).decode())
+    print(f"Maximum message size from server: {max_msg_size} bytes")
+
+    message_for_the_server = input("Enter message for the server: ")#צריך להשלים
+    client_socket.send(message_for_the_server.encode())#צריך להשלים
+
+
     client_socket.close()
-    print("the connected us closed")
+    print("the connected is closed")
 
 if __name__ == "__main__":
    client()
--- Server.py
+++ Server.py
@@ -1,17 +1,46 @@
 import socket
 
-def run_server():
+def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('127.0.0.1', 12345))
     server_socket.listen()
     print("Server is listening...")
 
+    #.....................max message size......................
+
+    max_message_size1 = get_value_from_file("a.txt", "maximum_msg_size")
+    try:
+        max_message_size2 = int(input("Enter max message size for the server: "))
+    except ValueError:
+        print("Invalid input. Using default max message size: 1024")
+        max_message_size2 = max_message_size1
+    max_message_size = max(max_message_size1, max_message_size2)
+    #..............................................................
+
     while True:
         client_socket, address = server_socket.accept()
         print(f"Connection established with {address}")
+
+        request = client_socket.recv(1024).decode()
+        if request == "GET_MAX_MSG_SIZE":
+            client_socket.send(str(max_message_size).encode())
+            print(f"max message size: {max_message_size}")
+        else:
+            print(f"Unknown request: {request}")
+
         client_socket.close()
-        print("the connected us closed")
+        print("the connected is closed")
+
+def get_value_from_file(file_name, variable_name):
+    with open(file_name, 'r') as file:
+        for line in file:
+            if line.startswith(f"{variable_name}:"):
+                value = line.strip().split(":", 1)[1]
+                if value.isdigit():
+                    return int(value.strip())
+                return value.strip()
+    return None
 
 if __name__ == "__main__":
-    run_server()
+   server()
 
